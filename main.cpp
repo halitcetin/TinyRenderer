7b9782b95f8e31a1583fe4417387b839485c88f9
Halit Cetin <cetin6u@fst-ats-311-l03.fst.site.univ-lorraine.fr>
2017-05-16 17:37:04 +0200

    forgot to commit lesson2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
+#include <vector>
 #include <iostream>
-#include <stdlib.h>
+#include <cmath>
 #include "lib/tgaimage.h"
 #include "common/drawer.h"
 #include "lib/geometry.h"
 #include "lib/model.h"
+#include <cstdlib>
+#include<cstdio>
 
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
@@ -40,60 +43,33 @@ void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
     }
 }
 
-/*
+
+
+Vec3f barycentric(Vec2i *pts, Vec2i P) {
+    Vec3f u = cross(Vec3f(pts[2][0]-pts[0][0], pts[1][0]-pts[0][0], pts[0][0]-P[0]), Vec3f(pts[2][1]-pts[0][1], pts[1][1]-pts[0][1], pts[0][1]-P[1]));
+    if (std::abs(u[2])<1){
+        return Vec3f(-1,1,1);
+    } else{
+        return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+    }
+}
+
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
-    // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!)
+    if (t0.y==t1.y && t0.y==t2.y) return;
     if (t0.y>t1.y) std::swap(t0, t1);
     if (t0.y>t2.y) std::swap(t0, t2);
     if (t1.y>t2.y) std::swap(t1, t2);
     int total_height = t2.y-t0.y;
-    for (int y=t0.y; y<=t1.y; y++) {
-        int segment_height = t1.y-t0.y+1;
-        float alpha = (float)(y-t0.y)/total_height;
-        float beta  = (float)(y-t0.y)/segment_height; // be careful with divisions by zero
+    for (int i=0; i<total_height; i++) {
+        bool second_half = i>t1.y-t0.y || t1.y==t0.y;
+        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
+        float alpha = (float)i/total_height;
+        float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height;
         Vec2i A = t0 + (t2-t0)*alpha;
-        Vec2i B = t0 + (t1-t0)*beta;
+        Vec2i B = second_half ? t1 + (t2-t1)*beta : t0 + (t1-t0)*beta;
         if (A.x>B.x) std::swap(A, B);
         for (int j=A.x; j<=B.x; j++) {
-            image.set(j, y, color); // attention, due to int casts t0.y+i != A.y
-        }
-    }
-    for (int y=t1.y; y<=t2.y; y++) {
-        int segment_height =  t2.y-t1.y+1;
-        float alpha = (float)(y-t0.y)/total_height;
-        float beta  = (float)(y-t1.y)/segment_height; // be careful with divisions by zero
-        Vec2i A = t0 + (t2-t0)*alpha;
-        Vec2i B = t1 + (t2-t1)*beta;
-        if (A.x>B.x) std::swap(A, B);
-        for (int j=A.x; j<=B.x; j++) {
-            image.set(j, y, color); // attention, due to int casts t0.y+i != A.y
-        }
-    }
-}
-*/
-
-Vec3f barycentric(Vec2i *pts, Vec2i P) {
-    Vec3f u = cross(Vec3f(pts[2][0]-pts[0][0], pts[1][0]-pts[0][0], pts[0][0]-P[0]), Vec3f(pts[2][1]-pts[0][1], pts[1][1]-pts[0][1], pts[0][1]-P[1]));
-    if (std::abs(u[2])<1) return Vec3f(-1,1,1); // triangle is degenerate, in this case return smth with negative coordinates
-    return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
-}
-
-void triangle(Vec2i *pts, TGAImage &image, TGAColor color) {
-    Vec2i bboxmin(image.get_width()-1,  image.get_height()-1);
-    Vec2i bboxmax(0, 0);
-    Vec2i clamp(image.get_width()-1, image.get_height()-1);
-    for (int i=0; i<3; i++) {
-        for (int j=0; j<2; j++) {
-            bboxmin[j] = std::max(0,        std::min(bboxmin[j], pts[i][j]));
-            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
-        }
-    }
-    Vec2i P;
-    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) {
-        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) {
-            Vec3f bc_screen  = barycentric(pts, P);
-            if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue;
-            image.set(P.x, P.y, color);
+            image.set(j, t0.y+i, color);
         }
     }
 }
@@ -107,38 +83,29 @@ int main(int argc, char** argv) {
         model = new Model(argv[1]);
     } else {
         model = new Model("obj/african_head.obj");
+    }
 
     TGAImage image(800, 800, TGAImage::RGB);
     image.clear(black);
 
-/*
-    for (int i=0; i<model->nfaces(); i++) {
-        std::vector<int> face = model->face(i);
-        for (int j=0; j<3; j++) {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j+1)%3]);
-            int x0 = (v0.x+1.)*width/2.;
-            int y0 = (v0.y+1.)*height/2.;
-            int x1 = (v1.x+1.)*width/2.;
-            int y1 = (v1.y+1.)*height/2.;
-            Drawer::line(x0, y0, x1, y1, image, white);
-        }
-*/
+
+
+    Vec3f light_dir(0,0,-1);
 
     for (int i=0; i<model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3];
-        Vec3f world_coords[3];
+        Vec2i coords_scr[3];
+        Vec3f coords_wld[3];
         for (int j=0; j<3; j++) {
             Vec3f v = model->vert(face[j]);
-            screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.);
-            world_coords[j]  = v;
+            coords_scr[j] = Vec2i((width/2.)*(v.x+1.), (height/2.)*(v.y+1.));
+            coords_wld[j] = v;
         }
-        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
-        n.normalize();
+    Vec3f n = (coords_wld[2]-coords_wld[0]) ^ (coords_wld[1]-coords_wld[0]);
+    n.normalize();
         float intensity = n*light_dir;
         if (intensity>0) {
-            triangle(screen_coords, image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
+            triangle(coords_scr, image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
         }
     }
 
