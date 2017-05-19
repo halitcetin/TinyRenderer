4e3c0a987f27bf7690b3e5cff667d37832fb4766
Halit Cetin <cetin6u@fst-ats-309-l13.fst.site.univ-lorraine.fr>
2017-05-19 17:02:42 +0200

    modifs

--- main.cpp
+++ main.cpp
@@ -54,6 +54,19 @@ Vec3f barycentric(Vec2i *pts, Vec2i P) {
     return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
 }
 
+Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
+    Vec3f s[2];
+    for (int i=2; i--; ) {
+        s[i][0] = C[i]-A[i];
+        s[i][1] = B[i]-A[i];
+        s[i][2] = A[i]-P[i];
+    }
+    Vec3f u = cross(s[0], s[1]);
+    if (std::abs(u[2])>1e-2)
+        return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+    return Vec3f(-1,1,1);
+}
+
 void triangle(Vec2i *pts, TGAImage &image, TGAColor color) {
     Vec2i bboxmin(image.get_width()-1,  image.get_height()-1);
     Vec2i bboxmax(0, 0);
@@ -76,6 +89,32 @@ void triangle(Vec2i *pts, TGAImage &image, TGAColor color) {
 }
 
 
+void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
+    Vec2f bboxmin( std::numeric_limits<float>::max(),  std::numeric_limits<float>::max());
+    Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+    Vec2f clamp(image.get_width()-1, image.get_height()-1);
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            bboxmin[j] = std::max(0.f,      std::min(bboxmin[j], pts[i][j]));
+            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
+        }
+    }
+    Vec3f P;
+    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) {
+        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) {
+            Vec3f bc_screen  = barycentric(pts[0], pts[1], pts[2], P);
+            if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue;
+            P.z = 0;
+            for (int i=0; i<3; i++) P.z += pts[i][2]*bc_screen[i];
+            if (zbuffer[int(P.x+P.y*width)]<P.z) {
+                zbuffer[int(P.x+P.y*width)] = P.z;
+                image.set(P.x, P.y, color);
+            }
+        }
+    }
+}
+
+
 
 Vec3f world2screen(Vec3f v) {
     return Vec3f(int((v.x+1.)*width/2.*+.5), int((v.y+1.)*height/2.+.5), v.z);
@@ -93,6 +132,10 @@ int main(int argc, char** argv) {
         model = new Model("obj/african_head.obj");
     }
 
+    float *zbuffer = new float[width*height];
+    for (int i=width*height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
+
+
     TGAImage image(width, height, TGAImage::RGB);
     image.clear(black);
 
@@ -108,9 +151,12 @@ int main(int argc, char** argv) {
             coor_w[j] = tmp;
         }
 
+
+
         Vec3f n = cross((coor_w[2]-coor_w[0]),(coor_w[1]-coor_w[0]));
         n.normalize();
         float intensity = n*light_dir;
+
         if (intensity>0) {
             triangle(coor_s, image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
         }
